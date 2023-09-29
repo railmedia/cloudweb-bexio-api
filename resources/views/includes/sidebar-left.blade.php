4eda592092a132a1bbf2447345900c95e250e9e1
railmedia <adrian.tudorache@cloudweb.ch>
2023-09-29 15:37:35 +0200

    1.0.0 home and login

--- resources/views/includes/sidebar-left.blade.php
+++ resources/views/includes/sidebar-left.blade.php
@@ -18,6 +18,11 @@
                         </li>
                         @endif
                         <li class="flex">
+                            <a href="{{ route('dashboard') }}" class="w-full py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
+                            <i class="fa-solid fa-house pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Home</span>
+                            </a>
+                        </li>
+                        <li class="flex">
                             <a href="{{ route('bexio.main') }}" class="w-full py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
                                 <i class="fas fa-tasks pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Bexio</span>
                             </a>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,8 +8,7 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.scss', 'resources/js/app.js'])
